73bc84924ca1db1e83c792ba5ac86a8f6f678aac
pshspsh <m13268047148@163.com>
2020-09-23 22:59:30 +0800

    leetcode note

---
+++ Week_09/Solution09.java
@@ -0,0 +1,56 @@
+import java.util.Arrays;
+
+public class Solution09 {
+    // 300. 最长上升子序列
+    public int lengthOfLIS(int[] nums) {
+        int len = nums.length;
+        if (len < 2) {
+            return len;
+        }
+
+        int[] dp = new int[len];
+        Arrays.fill(dp, 1);
+
+        for (int i = 1; i < len; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+
+        int res = 0;
+        for (int i = 0; i < len; i++) {
+            res = Math.max(res, dp[i]);
+        }
+        return res;
+    }
+
+    // 91. 解码方法
+    public int numDecodings(String s) {
+        int len = s.length();
+        int[] dp = new int[len + 1];
+        dp[len] = 1;
+        //最后一个数字不等于 0 就初始化为 1
+        if (s.charAt(len - 1) != '0') {
+            dp[len - 1] = 1;
+        }
+        for (int i = len - 2; i >= 0; i--) {
+            //当前数字时 0 ，直接跳过，0 不代表任何字母
+            if (s.charAt(i) == '0') {
+                continue;
+            }
+            int ans1 = dp[i + 1];
+            //判断两个字母组成的数字是否小于等于 26
+            int ans2 = 0;
+            int ten = (s.charAt(i) - '0') * 10;
+            int one = s.charAt(i + 1) - '0';
+            if (ten + one <= 26) {
+                ans2 = dp[i + 2];
+            }
+            dp[i] = ans1 + ans2;
+
+        }
+        return dp[0];
+    }
+}
